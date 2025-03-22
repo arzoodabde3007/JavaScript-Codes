56f6aa8565e082e20fdc23ffecc0a2c65acf7fd7
arzoodabde3007 <164984189+arzoodabde3007@users.noreply.github.com>
2025-03-22 20:51:23 +0000

    Type conversion in js

---
+++ Basics/L4_Conversion.js
@@ -0,0 +1,39 @@
+/* let score = "33";
+
+console.log(typeof(score));
+
+let validScore = Number(score);
+
+console.log(typeof(validScore));
+console.log(validScore); */
+
+/*let score = "33abc";
+
+let validScore = Number(score);
+
+console.log(typeof(validScore)); //number 
+console.log(validScore); //NaN
+// */
+
+//"33" => 33
+//"33abc" => NaN, type = number
+//true => 1, false => 0
+
+/* let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+console.log(typeof(booleanIsLoggedIn));
+*/
+
+//1 => true
+//0 => false
+//"" => false
+//"hitesh" => true
+
+let someNumber = 33; 
+let StringNum = String(someNumber);
+
+console.log(StringNum);
+console.log(typeof StringNum);
